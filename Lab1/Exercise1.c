1a0cfe0fae5b6a62d97f288bd853daad73f891b8
AlbertFaust <albertfaust1504@gmail.com>
2015-04-16 23:32:14 +0100

    Create Exercise1.c

---
+++ Lab1/Exercise1.c
@@ -0,0 +1,10 @@
+/*Laboratory Session 1 - Exercise 1: Hello World! (50%)
+Here is the minimal "Hello World!" program discussed in the lectures*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("Hello World!\n");
+	return(EXIT_SUCCESS);
+}
