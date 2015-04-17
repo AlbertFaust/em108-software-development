d0a67114765791bb0de9aa0033f2cc58c0eade5c
AlbertFaust <albertfaust1504@gmail.com>
2015-04-17 22:47:45 +0100

    Create Exercise3.c

---
+++ Lab2/Exercise3.c
@@ -0,0 +1,19 @@
+/*Laboratory Session 2 - Exercise 3: Geometric Figures (40%)
+Write a program that uses the following “print” statement:
+printf(”*\n**\n***\n****\n*****\n”);
+What does the program print?
+Next, print a square and a diamond. Use your initial instead of the “*” character.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	printf("Increase\n*\n**\n***\n****\n*****\n\n");
+	printf("Square\n*****\n*****\n*****\n*****\n*****\n\n");
+	printf("Diamond\n    *\n   ***\n  *****\n *******\n*********\n *******\n  *****\n   ***\n    *\n\n");
+	printf("Increase\nr\nrr\nrrr\nrrrr\nrrrrr\n\n");
+
+	return(0);
+}
