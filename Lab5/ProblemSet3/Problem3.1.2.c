e97ce92ed06f58c7c4475d5269c68df5292aa7fd
Albert Faust <albertfaust1504@gmail.com>
2015-04-27 17:04:50 +0100

    Update and rename Problem1.1.2.c to Problem3.1.2.c

--- Lab5/ProblemSet3/Problem3.1.2.c
+++ Lab5/ProblemSet3/Problem3.1.2.c
@@ -1,7 +1,7 @@
 /*Laboratory Session 5 - Problem Set No. 3 - Problem 3.1
 Write a program that uses a loop to display the numbers from a=1 to b=10, together with the square and cube of each of their values. 
 Redo the problem by reading values for a and b.
-(I have the first part of this problem here: https://github.com/AlbertFaust/em108-software-development/blob/master/Lab5/ProblemSet3/Problem1.1.1.c)*/
+(I have the first part of this problem here: https://github.com/AlbertFaust/em108-software-development/blob/master/Lab5/ProblemSet3/Problem3.1.1.c)*/
 
 #include <stdio.h>
 #include <math.h>
